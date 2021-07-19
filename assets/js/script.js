5dcf6f02c1e14cb77d4f0fc2d9ace08646808ecf
gtmpai <gtmpai@outlook.com>
2021-07-20 01:22:40 +0530

    Added 3/4 sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,62 @@
 $(document).ready(function() {
-
-
     $('#landing-image').hover(function() {
         $('#landing-image').attr('src', './assets/images/Bitmoji/2.png');
     }, function() {
         $('#landing-image').attr('src', './assets/images/Bitmoji/1.png');
     });
+
+
+
+    let navList = [];
+    $('#nav a').each(function(i) {
+        let thisLink = $(this);
+        let thisId = thisLink.attr('href');
+        let thisTarget = $(thisId);
+
+        navList.push({
+            'anchor': thisLink,
+            'id': thisId,
+            'target': thisTarget
+        });
+
+        thisLink.on('click', function(e) {
+            $('#nav .active').each(function(i) {
+                $(this).removeClass('active');
+            })
+            $(thisLink).addClass('active');
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: thisTarget.offset().top
+            }, 0);
+        })
+
+    })
+
+    $(document).on('scroll', function(e) {
+        $.each(navList, function(e, elem) {
+            let place = elem.target[0].getBoundingClientRect();
+            let anchor = elem.anchor[0];
+            if (place.top < window.innerHeight && place.bottom > 0) {
+                history.pushState({}, '', elem.id);
+
+                $('#nav .active').each(function(i) {
+                    $(this).removeClass('active');
+                })
+                $(anchor).addClass('active');
+
+                return false;
+            }
+        })
+    })
+
+
+    $(document).scroll(function() {
+        if ($(document).scrollTop() > 430) {
+            $('#navigation-reactive').removeClass('navigation-reactive');
+            $('#navigation-reactive').addClass('sticky-nav');
+        } else {
+            $('#navigation-reactive').addClass('navigation-reactive');
+            $('#navigation-reactive').removeClass('sticky-nav');
+        }
+    })
 });
